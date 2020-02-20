cfd46ca991cbf3f223ae7ef083cae35c6644663a
Ritesh <railsriteshc56@gmail.com>
2020-02-20 12:40:29 +0530

    Intialize category system

---
+++ db/migrate/20200217050043_create_attributes.rb
@@ -0,0 +1,6 @@
+class CreateAttributes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :attributes do |t|
+    end
+  end
+end
---
+++ db/migrate/20200217050202_create_saved_values.rb
@@ -0,0 +1,6 @@
+class CreateSavedValues < ActiveRecord::Migration[5.0]
+  def change
+    create_table :saved_values do |t|
+    end
+  end
+end
